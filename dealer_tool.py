2238f2e985339df3c65d33d51797c0a2fbfb5f9b
ryanblanchette <rblanchette@umass.edu>
2024-03-15 11:47:32 -0400

    night

--- dealer_tool.py
+++ dealer_tool.py
@@ -1,4 +1,5 @@
 '''----------------FILE FOR ALL OF THE FUNCTIONS NEED----------------'''
+from test7 import *
 def display_menu():
     """display the menu of options"""
     print("\n1-List; 2-Info; 3-Remove; 4-Insert; 5-Compare; 6-Search; 7-Discount")
@@ -7,6 +8,29 @@ def display_menu():
 #### To complete (all functions below)
 
 
+def carlist():
+    vehicle1=["Toyota","Camry","2018","45,000","20,000"]
+    vehicle2=["Ford","Escape","2019","30,000","23,500"]
+    vehicle3=["Honda","Accord","2017","60,000","16,200"]
+    vehicle4=["Chevrolet","Silverado","2020","25,000","41,000"]
+    vehicle5=["BMW","3 Series","2016","60,000","20,500"]
+    vehicle6=["Nissan","Rogue","2019","40,000","17,800"]
+    vehicle7=["Hyundai","Sonata","2018","42,000","16,500"]
+    vehicle8=["Jeep","Wrangler","2021","15,000","32,000"]
+    vehicle9=["Ford","Mustang","2015","50,000","22,000"]    
+    vehicle10=["Volkswagen","Golf","2017","38,000","17,800"]
+    vehicles=[vehicle1,vehicle2,vehicle3,vehicle4,vehicle5,vehicle6,vehicle7,vehicle8,vehicle9,vehicle10]
+    return(vehicles) 
+
+
+def discounted_vehicle(car_that_customer_want_discount, percent_discount, vehicleslist):
+    if percent_discount < 0 or percent_discount > 100:
+        print("Invalid discount!")
+    else:
+        vehicle = vehicleslist[int(car_that_customer_want_discount)-1]
+        new_discounted_price = float(vehicle[4])*float(percent_discount/100)
+        return new_discounted_price
+    return 0
 
 
 def display(vehicles_list):
@@ -16,7 +40,7 @@ def display(vehicles_list):
     while i < len(vehicles_list):
         vehicle = vehicles_list[i]
         print(vehicle[0],vehicle[1],vehicle[2],vehicle[3],vehicle[4],sep='\t\t')
-        i = i+1
+        i = i+1  #probaby shjould be -1 but i dont wanna change without confimring with ethan.
 
         # We probably need to write a program that loops here to iterate through every
         # variable in the vehicles_list, and display them during that process
@@ -37,3 +61,4 @@ def add_new_vehicle(vehicles_list):
     vehicles_list += new_car #adds a new car to the end of the list
     
     return vehicles_list #Vehicle added to the list.
+
--- project1.py
+++ project1.py
@@ -1,12 +1,5 @@
 '''----------------Import Modules----------------'''
 import dealer_tool as dealer
-from test1 import *
-import test2
-import test3
-import test4
-import test5
-import test6
-import test7
 
 '''----------------Main program starts here----------------'''
 print("\n==================================")
@@ -23,8 +16,8 @@ while True:
     if option=="":
         break
     ''' BEGIN MAKING YOUR CODE HERE '''
-    if option=="1":
-        exec(test1)
+    #if option=="1":
+        #test1()
     """if option=="2":
         test2
     if option=="3":
@@ -34,11 +27,20 @@ while True:
     if option=="5":
         test5
     if option=="6":            
-        test6
+        test6"""
     if option=="7":
-        test7"""
-
-
+        from dealer_tool import *
+        car = int(input("Which vehicle do you want to discount: "))
+        vehicles = carlist(car+1)
+        discount = input("What percent discount (0-100): ")   
+        print(car)
+        #new_discounted_price=vehicle(car). want to replace price with discounted price
+        #percant_discount=discount/100
+        #new_price=percant_discount * 
+        #print(new_price)
+        #break
+        #dealer.discounted_vehicle(car,discount,test7.vehicles)
+        #test7
 
 
 # Code reaches here after the loop ends
