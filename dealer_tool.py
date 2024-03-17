e8fbd78c60cd8b1140d0d368bed72dc5aa0ba2e6
etan8225 <eoconnor8225@gmail.com>
2024-03-17 17:11:19 -0400

    to get max points

--- dealer_tool.py
+++ dealer_tool.py
@@ -27,20 +27,21 @@ def display(vehicles_list):
 def info(vehicles_list):
     print("#Vehicles",str(len(vehicles_list)),"\n")
     
-    lessthan10,tento20,twentyTo30,thirtyTo40,fourtyup = 0,0,0,0,0       #to count how many cars are in each group
-    for i in vehicles_list:
-        if int(i[4]) < 10000:                                           #for loop goes through every car and catagorizes them,
-            lessthan10 = lessthan10+1                                   #saving the number of cars in each group
-        elif int(i[4]) >= 10000 and int(i[4]) < 20000:      
+    lessthan10,tento20,twentyTo30,thirtyTo40,fourtyup = 0,0,0,0,0         #to count how many cars are in each group
+    for car in vehicles_list:
+        if int(car[4]) < 10000:                                           #for loop goes through every car and catagorizes them,
+            lessthan10 = lessthan10+1                                     #saving the number of cars in each group
+        elif int(car[4]) >= 10000 and int(car[4]) < 20000:      
             tento20 = tento20+1
-        elif int(i[4]) >= 20000 and int(i[4]) < 30000:
+        elif int(car[4]) >= 20000 and int(car[4]) < 30000:
             twentyTo30 = twentyTo30+1
-        elif int(i[4]) >= 30000 and int(i[4]) < 40000:
+        elif int(car[4]) >= 30000 and int(car[4]) < 40000:
             thirtyTo40 = thirtyTo40+1
-        elif int(i[4]) >= 40000:
+        elif int(car[4]) >= 40000:
             fourtyup = fourtyup+1
-    print("<10,000 : ",100*(lessthan10/len(vehicles_list)),"%",sep="")  # then print the percentage in each group
-    print("10,000s : ",100*(tento20/len(vehicles_list)),"%",sep="")     # when compared to the total amount of cars
+    
+    print("<10,000 : ",100*(lessthan10/len(vehicles_list)),"%",sep="")    # then print the percentage in each group
+    print("10,000s : ",100*(tento20/len(vehicles_list)),"%",sep="")       # when compared to the total amount of cars
     print("20,000s : ",100*(twentyTo30/len(vehicles_list)),"%",sep="")
     print("30,000s : ",100*(thirtyTo40/len(vehicles_list)),"%",sep="")
     print(">=40,000 : ",100*(fourtyup/len(vehicles_list)),"%\n",sep="")
@@ -54,14 +55,9 @@ def info(vehicles_list):
         i = i + 1
     print("Mean Price:",str(total/len(vehicles_list)))
 
-def removeVehicle(vehiclesList):
-    while True:                                             #error check at bottom to ask what car wants to be removed again if invalid number is given
-        carID = int(input("Which vehicle do you want to remove (enter a number): "))
-        if carID <= len(vehiclesList):                       #carID is in the list
-            vehiclesList = (vehiclesList[:(carID-1)] + vehiclesList[carID:])
-            return vehiclesList
-        elif carID > len(vehiclesList) or carID < 0:         #carID is not in list
-            print("You entered an invalid number.")
+def removeVehicle(vehiclesList,carID):
+    del vehiclesList[carID-1] #delete the car from the list
+    return vehiclesList       #return the updated list
 
 def add_new_vehicle():
     Make = input("Enter Make: ")
@@ -73,12 +69,7 @@ def add_new_vehicle():
     
     return new_car                                          #Vehicle added to the vehiclesList list when it is passed back
 
-def compareVehicles(vehiclesList):
-    firstVehicle=int(input("First vehicle (enter a number):"))-1
-    secondVehicle=int(input("Second vehicle (enter a number):"))-1
-    vehicleInfo1=vehiclesList[firstVehicle]                 #set vehicleInfo to each selected option
-    vehicleInfo2=vehiclesList[secondVehicle]
-
+def compareVehicles(vehicleInfo1,vehicleInfo2):
     print(vehicleInfo1[0],vehicleInfo1[1],"vs.",vehicleInfo2[0],vehicleInfo2[1],) #printing the title
 
     vehiclemake1=vehicleInfo1[1]
@@ -90,57 +81,41 @@ def compareVehicles(vehiclesList):
 
     year1=int(vehicleInfo1[2])
     year2=int(vehicleInfo2[2])
-    if year1>year2:     #comparing year of the cars
+    if year1>year2:                                         #comparing year of the cars
         print("The first vehicle is newer.")
     elif year1<year2:
         print("The second vehicle is newer.")
     else:
-        print("They where made the same year.")
+        print("They where made the same year.")             #comparing cost of the cars
     if int(vehicleInfo1[4])>int(vehicleInfo2[4]):
         print("The first vehicle costs more.")
     elif int(vehicleInfo1[4])<int(vehicleInfo2[4]):
         print("The second vehicle costs more.")
     else:
-        print("The vehicles cost the same")
+        print("The vehicles cost the same.")
 
-def searchFunc(vehiclesList):
-    minprice=int(input("Please select a minimum price: "))
-    maxprice=int(input("Please select a maximum price: "))
-    print("")
+def searchFunc(vehiclesList,minprice,maxprice):
     count = 0
-    for i in range(0,len(vehiclesList),1):
-        currentvehicle=vehiclesList[i]
+    for i in range(0,len(vehiclesList),1):                  #check to see which cars match the price range
+        currentvehicle=vehiclesList[i]                      #and print them accordingly
         currentvehicleprice=int(currentvehicle[4])
         if currentvehicleprice >= minprice and currentvehicleprice <=maxprice:
             print(currentvehicle[0],currentvehicle[1],currentvehicle[4])
-            count=count+1
+            count=count+1                                   #increase the count to print number of cars
         else:
             continue    
-    print("\nTotal number of vehicles:",str(count))
+    print("\nTotal number of vehicles:",str(count))         #print number of cars
 
-def discountedCar(vehiclesList):
-    carNumber = int(input("Which vehicle do you want to discount: "))-1
-    while 0>carNumber or len(vehiclesList)-1<carNumber:
-        if 0<=carNumber<=len(vehiclesList)-1:
-            break
-        else:
-            print("vehicle not found")
-            carNumber = int(input("Which vehicle do you want to discount: "))-1  
-    discount = float(input("What percent discount (0-100): "))  
-    while 0>discount or 100<discount:
-        if 0<=discount<=100:
-            break
-        else:
-            print("invalid discount")
-            discount = float(input("What percent discount (0-100): ")) 
-    selectedVehicle = vehiclesList[carNumber]
+def discountedCar(vehiclesList,carNumber,discount):
+    selectedVehicle = vehiclesList[carNumber] #set vehicle to the selected vehicle
     print("Vehicle:",selectedVehicle[0])
 
-    priceVehicle = selectedVehicle[4]
+    priceVehicle = selectedVehicle[4] #set priceVehicle to the price
     print("Price:",priceVehicle)
     print("Discount:",((discount/100)*int(priceVehicle)))
 
     priceVehicle= int(priceVehicle)
-    newPrice=int(priceVehicle-(priceVehicle*discount/100))
+    newPrice=int(priceVehicle-(priceVehicle*discount/100)) #set newPrice to the discount
     print("New Price:",newPrice)
-    selectedVehicle[4]=str(newPrice)
+    selectedVehicle[4]=str(newPrice) #change the price of the vehicle
+    return selectedVehicle
--- project1.py
+++ project1.py
@@ -7,8 +7,8 @@ print("Dealership Vehicle Management Tool")
 print("==================================")
 print("")
 
-v1 =["Toyota",    "Camry",    "2018","45000","20000"]
-v2 =["Ford",      "Escape",   "2019","30000","23500"]
+v1 =["Toyota",    "Camry",    "2018","45000","20000"] #must run this here (or a function that does a similar job)
+v2 =["Ford",      "Escape",   "2019","30000","23500"] #to set up the default car list
 v3 =["Honda",     "Accord",   "2017","60000","16200"]
 v4 =["Chevrolet", "Silverado","2020","25000","41000"]
 v5 =["BMW",       "3 Series", "2016","60000","20500"]
@@ -35,19 +35,59 @@ while True:
         dealer.info(vehiclesList)                           # calls info function from dealer, displays required info
 
     if option=="3":
-        vehiclesList = dealer.removeVehicle(vehiclesList)   # removes user-selected car
+        while True:                                                         # error check at bottom to ask what car wants to be...
+            carID = int(input("Which vehicle do you want to remove (enter a number): "))  #...removed again if invalid number is given
+            if carID <= len(vehiclesList):                                  # carID is in the list
+                vehiclesList = dealer.removeVehicle(vehiclesList,carID)     # removes user-selected car
+                break
+            elif carID > len(vehiclesList) or carID < 0:         # carID is not in list
+                print("You entered an invalid number.")
+                continue
     
     if option=="4":
-        vehiclesList = [dealer.add_new_vehicle()]           # adds a new vehicle to the list
+        vehiclesList = vehiclesList+[dealer.add_new_vehicle()]           # adds a new vehicle to the list
 
     if option=="5":
-        dealer.compareVehicles(vehiclesList)                # compares two user selected vehicles
+        while True:            #error check to make sure the number provided makes sense, if so,
+            firstVehicle=int(input("First vehicle (enter a number): "))-1 #continue running the program
+            if firstVehicle<=len(vehiclesList):
+                break
+            else:
+                print("Invalid input, retry.")
+                continue
+        while True:
+            secondVehicle=int(input("Second vehicle (enter a number): "))-1
+            if secondVehicle<=len(vehiclesList):
+                break
+            else:
+                print("Invalid input, retry.")
+                continue
+        vehicleInfo1=vehiclesList[firstVehicle]                 # set vehicleInfo to the selected numbers
+        vehicleInfo2=vehiclesList[secondVehicle]
+        dealer.compareVehicles(vehicleInfo1,vehicleInfo2)       # calls func with both vehicles info
 
-    if option=="6":            
-        dealer.searchFunc(vehiclesList)                     # searches for vehicles in a certain price range
+    if option=="6":
+        minprice=int(input("Please select a minimum price: "))
+        maxprice=int(input("Please select a maximum price: ")) # get the min and max prices
+        print("")            
+        dealer.searchFunc(vehiclesList,minprice,maxprice)      # searches for vehicles in a certain price range
 
     if option=="7":
-        dealer.discountedCar(vehiclesList)                  # discounts a user-selected car
+        while True:         #error checking to see if number inputted is invalid
+            carNumber = int(input("Which vehicle do you want to discount: "))-1
+            if carNumber>len(vehiclesList):
+                print("Invalid car number. Retry.")
+                continue
+            elif carNumber>=0 and carNumber<len(vehiclesList):
+                break
+        while True:         #same here, error checking to see if number is invalid
+            discount = float(input("What percent discount (0-100): "))
+            if 0<=discount<=100:
+                break
+            elif discount>100 or discount<0:
+                print("Invalid discount amount. Retry.")
+                continue
+        vehiclesList[carNumber] = dealer.discountedCar(vehiclesList,carNumber,discount)                  # discounts a user-selected car
 
 
 
