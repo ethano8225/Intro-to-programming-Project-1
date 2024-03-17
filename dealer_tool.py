9cebb7f06f825063acfa46f111541bfd34a366bf
etan8225 <eoconnor8225@gmail.com>
2024-03-16 20:18:52 -0400

    Update dealer_tool.py

--- dealer_tool.py
+++ dealer_tool.py
@@ -11,26 +11,26 @@ def display(vehicles_list):
     print("Car #"+"\t"+"Make","Model","Year","Mileage","Price($)",sep="\t\t")
     print("---------------------------------------------------------------------------------")
     i = 0
-    while i < len(vehicles_list):    # while loop prints all vehicles in list
+    while i < len(vehicles_list):                                                                         # while loop prints all vehicles in list
         vehicle = vehicles_list[i]   
-        if len(vehicle[0]) > 8 and len(vehicle[1]) >= 8:                                  #must check the length of the each string to allow for 
-            print(str(i+1),vehicle[0],vehicle[1],vehicle[2]+"\t\t"+vehicle[3]+"\t\t"+vehicle[4],sep="\t") #good looking formatting
-        elif len(vehicle[0]) > 8 and len(vehicle[1]) < 8:
+        if len(vehicle[0]) > 8 and len(vehicle[1]) >= 8:                                                  # must check the length of the each string to allow for 
+            print(str(i+1),vehicle[0],vehicle[1],vehicle[2]+"\t\t"+vehicle[3]+"\t\t"+vehicle[4],sep="\t") # good looking formatting
+        elif len(vehicle[0]) > 8 and len(vehicle[1]) < 8:                                                   
             print(str(i+1)+"\t"+vehicle[0]+"\t"+vehicle[1],vehicle[2],vehicle[3],vehicle[4],sep="\t\t")
         elif len(vehicle[0]) <= 8 and len(vehicle[1]) >= 8:
             print(str(i+1)+"\t"+vehicle[0],vehicle[1]+"\t"+vehicle[2],vehicle[3],vehicle[4],sep="\t\t")
         else:
             print(str(i+1)+"\t"+vehicle[0],vehicle[1],vehicle[2],vehicle[3],vehicle[4],sep='\t\t')
         i = i+1
-    return 0
 
 
 def info(vehicles_list):
     print("#Vehicles",str(len(vehicles_list)),"\n")
-    lessthan10,tento20,twentyTo30,thirtyTo40,fourtyup = 0,0,0,0,0 #to count how many cars are in each group
+    
+    lessthan10,tento20,twentyTo30,thirtyTo40,fourtyup = 0,0,0,0,0       #to count how many cars are in each group
     for i in vehicles_list:
-        if int(i[4]) < 10000:                               #for loop goes through every car and catagorizes them,
-            lessthan10 = lessthan10+1                       #saving the number of cars in each group
+        if int(i[4]) < 10000:                                           #for loop goes through every car and catagorizes them,
+            lessthan10 = lessthan10+1                                   #saving the number of cars in each group
         elif int(i[4]) >= 10000 and int(i[4]) < 20000:      
             tento20 = tento20+1
         elif int(i[4]) >= 20000 and int(i[4]) < 30000:
@@ -39,10 +39,8 @@ def info(vehicles_list):
             thirtyTo40 = thirtyTo40+1
         elif int(i[4]) >= 40000:
             fourtyup = fourtyup+1
-
-                                                                        # then print the percentage in each group
-    print("<10,000 : ",100*(lessthan10/len(vehicles_list)),"%",sep="")  # when compared to the total amount of cars
-    print("10,000s : ",100*(tento20/len(vehicles_list)),"%",sep="")
+    print("<10,000 : ",100*(lessthan10/len(vehicles_list)),"%",sep="")  # then print the percentage in each group
+    print("10,000s : ",100*(tento20/len(vehicles_list)),"%",sep="")     # when compared to the total amount of cars
     print("20,000s : ",100*(twentyTo30/len(vehicles_list)),"%",sep="")
     print("30,000s : ",100*(thirtyTo40/len(vehicles_list)),"%",sep="")
     print(">=40,000 : ",100*(fourtyup/len(vehicles_list)),"%\n",sep="")
@@ -50,8 +48,8 @@ def info(vehicles_list):
     total = 0
     i = 0
     while i < len(vehicles_list):           # go through every vehicle, get the price 
-        vehicle = vehicles_list[i]          # count the total price of every divided by the 
-        price = int(vehicle[4])             # amount of cars in the list
+        vehicle = vehicles_list[i]          # count the total price of every car divided by 
+        price = int(vehicle[4])             # the amount of cars in the list
         total = total + price
         i = i + 1
     print("Mean Price:",str(total/len(vehicles_list)))
